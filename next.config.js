2220b332fe452dc70b149c269ef06530e8cc77f6
satya subudhi <satyasubudhi089@gmail.com>
2026-03-22 23:25:58 +0530

    feat: set basePath and assetPrefix for deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/creative-portfolio',
+  assetPrefix: '/creative-portfolio/',
   transpilePackages: ['framer-motion'],
   images: {
     remotePatterns: [
